69301e799160c1f38862c7e9adcfbddbfca966c3
Jon Nordby <jononor@gmail.com>
2016-11-22 14:49:34 +0100

    Move non-core tools to own repository
    Moved to https://github.com/jonnor/agree-tools

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ agree.conditions = require('./src/conditions');
 agree.introspection = require('./src/introspection');
 agree.schema = require('./src/schema'); // XXX: core or no core?
 
-// TEMP: should move to agree-tools
-agree.doc = require('./src/doc');
-agree.testing = require('./src/testing');
-agree.analyze = require('./src/analyze');
-
 agree.express = require('./src/express'); // TEMP
 agree.chain = require('./src/chain'); // TEMP
 agree.Chain = agree.chain.Chain
